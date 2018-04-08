d25253763808a599f9574abe9536dd4c7683c30e
Erick Cordero Rojas <eguicoro2@gmail.com>
2018-04-08 17:40:03 -0600

    Unificaci on de componentes

--- SumaPF.sv
+++ SumaPF.sv
@@ -42,13 +42,66 @@ module SumaPF(input logic [31:0] A,
 	assign control2 = expRes[7];
 	
 	//MUX mantisas 1
-	MUX
-	muxOut(m10, m11, control1, resultMux1);
+	MUXM
+	muxM1(m10, m11, control1, resultMux1);
 		
 	//MUX mantisas 2
-	MUX
-	muxOut(m20, m21, control2, resultMux2);
+	MUXM
+	muxM2(m20, m21, control2, resultMux2);
 	
+	//resultado para el complemento
+	logic[7: 0] expComp;
+	
+	//complemento
+	complement
+	complement(expRes,expComp);
+	
+	//shift
+	srlN
+	srlN(resultMux2,expComp);
+	
+	//variables para la suma
+	logic carryin;
+	assign carryin = 0;
+	logic carryout;
+	logic [22:0] sumaRes;
+	 
+	//suma de mantisas
+	sumadorNbits 
+	#(8)
+	sumaN(resultMux1,resultMux2,carryin,carryout,sumaRes);
+	 
+	//and del bit de signo
+	logic bitSigno;
+	assign bitSigno = sumaRes[22];
+	logic andRes;
+	AND
+	AND(bitSigno,1,andRes);
+	
+	//shift
+	srlN
+	alineamiento(sumaRes,andRes);
+	
+	//variables para muxE
+	logic [7:0] mE0,mE1;
+	logic [7:0] resultMuxE;
+	logic controlE;
+	assign controlE = expRes[7];
+	
+	//mux exponentes
+	MUXE
+	muxE(mE0, mE1, control2, resultMux2);
+	
+	//variables para la suma
+	logic carryin2;
+	assign carryin2 = andRes;
+	logic carryout2;
+	logic [22:0] sumaResF;
+	
+	//suma para alinearr
+	sumadorNbits 
+	#(8)
+	sumaA(resultMuxE,0,carryin2,carryout2,sumaResF);
 	
 endmodule
 	
